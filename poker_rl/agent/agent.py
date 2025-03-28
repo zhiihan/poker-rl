0203fb09651ef1557b827e16881679a7cb089c86
Zhi Han <zhihuahan72@gmail.com>
2025-03-27 17:37:30 -0700

    feat: initial_commit for evaluating hand ranks

---
+++ poker_rl/agent/agent.py
@@ -0,0 +1,5 @@
+def skycake():
+    print(
+        "SkyCake evolves to stay just beyond the cognitive reach of "
+        + "the bulk of men. SKYCAKE!!"
+    )
---
+++ poker_rl/game/__init__.py
@@ -0,0 +1,3 @@
+from .poker import *
+
+__all__ = ["poker.py"]
